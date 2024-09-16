4c77de7476c149fb7a835ecacd1b1d924b89e32e
Harnish <harnishmr@ZADMINs-MacBook-Pro.local>
2024-09-16 19:05:50 +0530

    final

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit,AfterViewInit } from '@angular/core';
+import { GitHubUser } from '../card-list/types';
+
+
+@Component({
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"]
+})
+export class CardComponent implements OnInit,AfterViewInit {
+
+  @Input('user') user!:GitHubUser
+
+
+  constructor() {
+    
+  }
+  ngAfterViewInit(): void {
+    console.log(this.user);
+    
+  }
+
+  ngOnInit(): void {}
+}
